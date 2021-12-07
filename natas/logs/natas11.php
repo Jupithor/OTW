8f6d03e35e82f77ab9e7dc28ee8ec64d3376aa15
Thor <tryvard@gmail.com>
2021-12-07 09:13:41 +0100

    At level 12

---
+++ natas/logs/natas11.php
@@ -0,0 +1,33 @@
+<?php
+$defaultdata = array( "showpassword"=>"yes", "bgcolor"=>"#ffffff");
+$cookie=base64_decode('ClVLIh4ASCsCBE8lAxMacFMZV2hdVVotEhhUJQNVAmhSEV4sFxFeaAw%3D');
+
+function xor_encrypt($in) {
+    $key = json_encode(array( "showpassword"=>"no", "bgcolor"=>"#ffffff"));
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+function xor_decrypt($in) {
+    $key = 'qw8J';
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+print xor_encrypt($cookie);
+print("\n");
+print base64_encode(xor_decrypt(json_encode($defaultdata)))
+?>
